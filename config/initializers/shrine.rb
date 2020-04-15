690bbb705ea4dcd0811684370b34ed9a52be186c
Jim Miller <jemiller1963@gmail.com>
2020-04-15 18:00:01 -0400

    Removed region to test Heroku

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -4,7 +4,7 @@ s3_options = {
   bucket:            'ink3',
   access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
   secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-  region:            Rails.application.credentials.dig(:aws, :region)
+  region:            'us-east-1'
 }
 
 Shrine.storages = {
