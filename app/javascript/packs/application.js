ab2c6ab99c28e28e6e3d487514939a640dafda9c
Jim Miller <jemiller1963@gmail.com>
2020-05-08 14:29:29 -0400

    Updated JQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('stylesheets/application.scss')
-require('jquery')
-require("packs/notes")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("stylesheets/application.scss");
+require("jquery");
+require("packs/notes");
 window.Jquery = $;
 window.$ = $;
 
@@ -19,3 +19,7 @@ window.$ = $;
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on("turbolinks:load", function () {
+  console.log("Yeeha,we are ready");
+});
